9a36c91508e8fcb67da46944838adf17d3f239d6
Tom Horsman <thomashorsman99@gmail.com>
2025-06-13 17:07:19 +0100

    improved layout, add comments, moved the 3 checks into single function

--- day-02/day-02/d2_attempt(04){success}.py
+++ day-02/day-02/d2_attempt(04){success}.py
@@ -1,116 +1,112 @@
 
-def checkDiff(row):
+def checkRowDiff(row): # takes in a list of digits, iterates over the Nth, Nth + 1 pairs, if differnce between them are not between 1 and 3, return fail to program
     for item in range(len(row)-1):
-        a,b = row[item],row[item+1]
-        # print(a,b)
 
+        a,b = row[item],row[item+1] # gets Nth, Nth + 1 pair
+        # print(a,b)
         diff = abs(a - b)
 
         if diff > 3 or diff <=0:
             return False
+       
         else:
             continue
 
     return True # if no pairs diff are outside 1 to 3: then the row is safe
 
-def getDirection(row):
+
+def getDirection(row): # takes in a list of digits, iterates over the Nth, Nth + 1 pairs, the first uneven pair desides the direction for the rest of the list
     for item in range(len(row)-1):
         a,b = row[item],row[item+1]
 
-        if a > b:
+        if a > b: # e.g. 5,2
             return "decsend"
         
-        elif a < b:
+        elif a < b: # e.g. 1,4
             return "acsend"
-        
+
         else:
-            continue # if a and b are the same, keep iterating and checking for until a direction is found
+            continue # if a and b are the same, keep iterating and checking  until a direction is found
 
 
-
-
-def checkDirection(row,direction):
+def checkDirection(row,direction): #takes in a list of digits and a string, the directional string gathered from "getDirection" determines how the row is checked
     if direction == "acsend":
-        if sorted(row) == row:
+        if sorted(row) == row: # if a row is already in asending order, sorting the list will return the same value as the orginal
             return True
         
         else:
             return False
     
     elif direction == "decsend":
-        if sorted(row,reverse=True) == row:
+        if sorted(row,reverse=True) == row: # same logic as acsending, but with the sorted list reversed
             return True
         
         else:
             return False
 
 
-def secondCheck(row):
-    original = row
+def secondCheck(row): # takes in a list of digits, iterates over the list, creating a temp list with only the Nth digit removed, it then checks if that row is now safe, if yes, return TRUE to program
+    original = row # saves original input
 
-    for item in range(len(original)):
-        temp = original[:]
-        temp.pop(item)
+    for item in range(len(original)): 
+        temp = original[:] # craete a soft copy
+        temp.pop(item) # remove only the Nth item
 
+        result = performChecks(temp) # function that performs the checks
 
-        result = performSecondCheck(temp)
-
-        if result == True:
-            print(f"{original} can be safe >>> {temp}")
+        if result == True: 
+            print(f"{original} can be made safe >>> {temp}\n")
             return True
         
-def performSecondCheck(newRow):
 
-    direction = getDirection(newRow)
+def performChecks(newRow): # takes in a list of digits, calls 3 functions, and if the row is safe, returns TRUE to the program
 
-    outputDiff = checkDiff(newRow)
+    is_Diff_Safe = checkRowDiff(newRow) 
+    get_Direction = getDirection(newRow) 
+    is_Direction_Safe = checkDirection(newRow,get_Direction)
 
-    outputDirection = checkDirection(newRow,direction)
 
-
-    if outputDiff and outputDirection == True:
+    if is_Diff_Safe and is_Direction_Safe == True:
         return True
     else:
-        return None
+        return False
 
 
 
-with open("data2.txt", "r") as file:  # opens file
+with open("data2.txt", "r") as file:  # opens the data.txt file
     content = file.read()
 
-formatedList = []
-safeCount = 0
-failCount = 0
-splitConent = content.splitlines()
+formatedList = [] # container for the parsed data
 
-for numbs in splitConent:
+safeCount = 0 # counts the number of safe and unsafe rows
+failCount = 0
+
+
+splitConent = content.splitlines() # parses the data
+for numbs in splitConent: 
     formatedList.append(numbs.split(" "))
 
 dataList = [list(map(int, i)) for i in formatedList]
 
 
-for row in range(len(dataList)):
+for row in range(len(dataList)): # for row in data2.txt, 
 
-    currentRow = dataList[row]
+    currentRow = dataList[row] # gets the Nth row    
+    
 
-    direction = getDirection(currentRow)
+    is_Row_Safe = performChecks(currentRow) # checks if the row is safe
 
-    outputDiff = checkDiff(currentRow)
 
-    outputDirection = checkDirection(currentRow,direction)
-
-    # print(f"{dataList[row]}| DIRECTION = {direction}| DIFF = {outputDiff}| CONSIST = {outputDirection}")
-
-    if outputDiff == True and outputDirection == True:
+    if is_Row_Safe == True: 
         print(f"{row} {dataList[row]} = SAFE")
         safeCount += 1
 
     else:
         print(f"{row} {dataList[row]} == FAIL")
 
-        output = secondCheck(currentRow)
+        output = secondCheck(currentRow) # if the row is not safe, check if it can be made safe by removing a single item from the row
 
-        if output == True:
+        if output == True: # if removing a single digit can make the row safe, increament the safe counter, otherwise the row is not safe
             safeCount += 1
 
         else:
@@ -118,4 +114,4 @@ for row in range(len(dataList)):
 
         
 
-print(safeCount, failCount)
+print(safeCount, failCount) # prints the final count of safe and unsafe rows
