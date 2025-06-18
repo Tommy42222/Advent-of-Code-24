12de300ea51e509d02523b61bb245598aa510120
Tom Horsman <thomashorsman99@gmail.com>
2025-06-18 19:53:32 +0100

    improved Configurability and rename some functions

--- day-04/d4_01_p1.py
+++ day-04/d4_01_p1.py
@@ -3,10 +3,10 @@ import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from functions.homeGrownFunctions import openFile
 
-def get_X(content):
+def get_Match_coords(content,search_Character):
 
     x_Match_list = [] # dict to store all matches and their coordinates
-    pattern = re.compile('X') # search pattern
+    pattern = re.compile(search_Character) # search pattern
     matches = pattern.finditer(content) 
 
     for match in matches: 
@@ -37,8 +37,8 @@ def Search_for_XMAS(x_cord,y_cord,text,target_Characters_list):
 
 
 def check_angle(x_cord,y_cord,x_mult,y_mult,text,target_Characters_list):
-    target_Character = target_Characters_list
-    print(target_Character)
+    target_Characters = target_Characters_list
+
     try:
         for i in range(1,len(target_Characters_list)+1):
             x = x_cord + (i * x_mult)
@@ -55,7 +55,7 @@ def check_angle(x_cord,y_cord,x_mult,y_mult,text,target_Characters_list):
             
 
             # print("X",x_cord + (i * x_mult),"Y",y_cord + (i * y_mult),"| i =",i)
-            bool_value = text[y_cord + (i * y_mult)][x_cord + (i * x_mult)] == target_Character[i-1]
+            bool_value = text[y_cord + (i * y_mult)][x_cord + (i * x_mult)] == target_Characters[i-1]
             # print(bool_value, target_Character[i-1])
             
             if bool_value == False:
@@ -66,10 +66,10 @@ def check_angle(x_cord,y_cord,x_mult,y_mult,text,target_Characters_list):
         return 1
 
 
-        return 1
+
     except IndexError:
         # print("error")
-    
+        print(f"error")
         return 0
 
 
@@ -82,30 +82,22 @@ file = openFile("data4")
 newfile = file.splitlines()
 
 horizontal_Matches = 0
-# horizontal_Matches = file.count('XMAS') + file.count("SAMX")
-# print(f"Number of horizontal matches = {horizontal_Matches}")
 number0fRows = file.count("\n") 
 print(f"NUMBER OF ROWS = {number0fRows}")
 
 
 
-xList = []
+match_coord_List = []
 for row in range (number0fRows):
-     xList.append(get_X(newfile[row]))
+     match_coord_List.append(get_Match_coords(newfile[row],search_Character="X"))
 
-# pprint.pprint(xList)
-# print("---")
-# pprint.pprint(newfile)
-
-
-# print(xList)
 number_of_Hits = 0
 
 for row in range(0,number0fRows):
-    print(xList[row])
-    for item in range(0,len(xList[row])):
+    print(f"Row__[{row}]_{match_coord_List[row]}")
+    for item in range(0,len(match_coord_List[row])):
     
-        x_Location = xList[row][item]
+        x_Location = match_coord_List[row][item]
 
         x_Character = (newfile[row][x_Location])
 
@@ -116,7 +108,8 @@ for row in range(0,number0fRows):
         
 
    
-        print(number_of_Hits)
+    print(f"Current Total = {number_of_Hits}")
+
 print(f"Final Total = {number_of_Hits}")
 
    
