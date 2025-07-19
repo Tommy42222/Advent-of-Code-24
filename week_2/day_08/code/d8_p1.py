80235b9d5de56e082ff595ddf526a7cb5320b482
Tom Horsman <thomashorsman99@gmail.com>
2025-07-19 11:42:47 +0100

    add main(), changed how coordinates are stored in dict to improve access later

--- week_2/day_08/code/d8_p1.py
+++ week_2/day_08/code/d8_p1.py
@@ -13,18 +13,36 @@ def find_antennas(content):
         for row_index,item in enumerate(row):
             if item != ".":
 
-                coordinates = f"{column_index}:{row_index}"
+                coordinates = []
+                coordinates.append(column_index)
+                coordinates.append(row_index)
+                
                 antennas_locations_dict[item].append(coordinates)
 
-    pprint.pprint(antennas_locations_dict)
+    # pprint.pprint(antennas_locations_dict)
     return antennas_locations_dict
 
 
 
 
-here = os.path.dirname(__name__)
-with open (f"{here}../input/input8.txt", "r") as file:
-    content = file.read()
 
-parsed_content = parse_input(content)
-find_antennas(parsed_content)
+
+here = os.path.dirname(__name__)
+with open (f"{here}../input/sample8.txt", "r") as file:
+    content = file.read()
+    
+def main(content):
+    max_grid_bounds_limit = 50
+
+
+    parsed_content = parse_input(content)
+    antennas_coordinates_dict = find_antennas(parsed_content)
+
+
+    for antenna in antennas_coordinates_dict.values():
+        print(antenna)
+        pass
+
+
+
+main(content)
