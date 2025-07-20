e53ce313ae8ae0ed39f97ae09b8c1ccc1bb77efa
Tom Horsman <thomashorsman99@gmail.com>
2025-07-20 13:47:51 +0100

    add iteration over each coord pair in list, also add get differnce between two coords{check if antinode can exist next}

--- week_2/day_08/code/d8_p1.py
+++ week_2/day_08/code/d8_p1.py
@@ -1,4 +1,4 @@
-import os,re,pprint
+import os,pprint
 from collections import defaultdict
 
 def parse_input(file):
@@ -19,30 +19,70 @@ def find_antennas(content):
                 
                 antennas_locations_dict[item].append(coordinates)
 
-    # pprint.pprint(antennas_locations_dict)
+    pprint.pprint(antennas_locations_dict)
     return antennas_locations_dict
 
 
 
+def get_coordinate_differnce(first,second):
+    coordinate_differnces = []
+
+    print(f'INPUT = {first}, {second}')
+    x1 = first[0] 
+    y1 = first[1]
+
+    x2 = second[0]
+    y2 = second[1]
+
+    x_differnce = (x1 - x2) * -1
+    y_differnce = (y1 - y2) * -1
+
+    coordinate_differnces.append(x_differnce)
+    coordinate_differnces.append(y_differnce)
+
+    print(f"DIFFERNCE = {coordinate_differnces}\n")
+    return coordinate_differnces
 
 
 
-here = os.path.dirname(__name__)
-with open (f"{here}../input/sample8.txt", "r") as file:
-    content = file.read()
-    
+def iterate_over_antenna_signal(coordinates):
+    orginal_coordinates_list = coordinates
+
+    for index,item in enumerate(orginal_coordinates_list):
+
+        temp_list = orginal_coordinates_list[:]
+        temp_list.pop(index)
+        
+        for next_item in temp_list:
+            # print(item,next_item)
+            
+            differences = get_coordinate_differnce(item,next_item)
+
+
+def check_for_anti_node(next_item,differnces,grid):
+    pass
+
+
+
 def main(content):
-    max_grid_bounds_limit = 50
 
+    max_grid_bounds_limit = 50
 
     parsed_content = parse_input(content)
     antennas_coordinates_dict = find_antennas(parsed_content)
 
 
-    for antenna in antennas_coordinates_dict.values():
-        print(antenna)
-        pass
+    for signal,coordinates in antennas_coordinates_dict.items():
+
+            print(signal)
+            iterate_over_antenna_signal(coordinates)
+            print("-------")
 
 
 
+here = os.path.dirname(__name__)
+with open (f"{here}../input/input8.txt", "r") as file:
+    content = file.read()
+    
 main(content)
+
