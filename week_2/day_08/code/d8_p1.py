5b07019eb80f868df2ea97e98a70f7b081e63af5
Tom Horsman <thomashorsman99@gmail.com>
2025-07-24 10:39:27 +0100

    workning solution for part 1

--- week_2/day_08/code/d8_p1.py
+++ week_2/day_08/code/d8_p1.py
@@ -1,8 +1,9 @@
 import os,pprint
 from collections import defaultdict
 
+
 def parse_input(file):
-    output = file.splitlines()
+    output = [list(line) for line in file.splitlines()]
     return output
     
 
@@ -27,7 +28,6 @@ def find_antennas(content):
 def get_coordinate_differnce(first,second):
     coordinate_differnces = []
 
-    print(f'INPUT = {first}, {second}')
     x1 = first[0] 
     y1 = first[1]
 
@@ -40,12 +40,50 @@ def get_coordinate_differnce(first,second):
     coordinate_differnces.append(x_differnce)
     coordinate_differnces.append(y_differnce)
 
-    print(f"DIFFERNCE = {coordinate_differnces}\n")
+    # print(f'INPUT = {first}, {second}')
+    # print(f"DIFFERNCE = {coordinate_differnces}")
     return coordinate_differnces
 
 
 
-def iterate_over_antenna_signal(coordinates):
+def check_for_anti_node(next_item,differnces,grid,antenna_frequency):
+    global total_antinode_couter
+    
+    map_grid = grid
+
+    new_y_cord = next_item[0] + differnces[0]
+    new_x_cord = next_item[1] + differnces[1]
+
+    if new_x_cord < 0 or new_y_cord < 0:
+        print(f"less then 0", end=", ")
+
+    else:
+        try:
+            place_on_grid = map_grid[new_y_cord][new_x_cord]
+
+            if place_on_grid == "$":
+                print("already taken", end=", ")
+            # print(f"NEXT ITEM = {next_item}, DIFF = {differnces}, NEW_CORDS = {new_y_cord}:{new_x_cord}, FREQUANCY = {antenna_frequency}, NEW SPOT = {place_on_grid}")
+
+            else:
+                print("hit", end=", ")
+                total_antinode_couter += 1
+                map_grid[new_y_cord][new_x_cord] = "$"
+                
+
+        except IndexError:
+            print(f"out of bounds", end=", ")
+
+    print(f"INDEX = {new_y_cord}:{new_x_cord}")
+    return map_grid
+
+
+
+
+
+
+
+def iterate_over_antenna_signal(coordinates,grid,singal):
     orginal_coordinates_list = coordinates
 
     for index,item in enumerate(orginal_coordinates_list):
@@ -57,32 +95,37 @@ def iterate_over_antenna_signal(coordinates):
             # print(item,next_item)
             
             differences = get_coordinate_differnce(item,next_item)
+            grid = check_for_anti_node(next_item,differences,grid,singal)
+
+        # print("\n")
+    # pprint.pprint(grid)
 
 
-def check_for_anti_node(next_item,differnces,grid):
-    pass
 
 
 
 def main(content):
-
-    max_grid_bounds_limit = 50
-
     parsed_content = parse_input(content)
     antennas_coordinates_dict = find_antennas(parsed_content)
-
 
     for signal,coordinates in antennas_coordinates_dict.items():
 
             print(signal)
-            iterate_over_antenna_signal(coordinates)
+            iterate_over_antenna_signal(coordinates,parsed_content,signal)
             print("-------")
 
 
 
-here = os.path.dirname(__name__)
-with open (f"{here}../input/input8.txt", "r") as file:
-    content = file.read()
+
+if __name__ == "__main__":
+    here = os.path.dirname(__file__)
+    with open (f"{here}/../input/input8.txt", "r") as file:
+        content = file.read()
+
+    total_antinode_couter = 0
+    main(content)
+    print(total_antinode_couter)
+
     
-main(content)
+
 
