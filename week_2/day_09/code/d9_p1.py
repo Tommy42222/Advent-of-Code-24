7854aed12d58982529704cd50446ec0278165e04
Tom Horsman <thomashorsman99@gmail.com>
2025-07-28 17:57:16 +0100

    craete 2 functions: file now produse solution for day_09 part 1 1: swap_items_at_pointers(), this takes the items at the left and right pointers and swaps them 2: calculate_check_sum(), this will use the reordered disk to calculate the puzzle output
    - reorder_disk_map() now strips the list of all uneeded blankspace once the list is sorted before returning
    - improve how output is printed to terminal

--- week_2/day_09/code/d9_p1.py
+++ week_2/day_09/code/d9_p1.py
@@ -1,5 +1,5 @@
 
-import os,sys
+import os
 
 
 def generate_disk_map(file_input:str) -> list:
@@ -7,7 +7,7 @@ def generate_disk_map(file_input:str) -> list:
     data_index = 0
 
     for char_index, character in enumerate(file_input):
-        
+
         if char_index % 2 == 1: 
             for i in range(int(character)):
                 disk_map.append(".")
@@ -20,20 +20,28 @@ def generate_disk_map(file_input:str) -> list:
     return disk_map
 
 
+
 def move_left_pointer(disk_map:list, current_pointer_poition:int) -> int:
         for pointer_index, item in enumerate(disk_map[current_pointer_poition:]):
             if item == ".":
-                print(pointer_index, item)
-                return pointer_index
-
+                # print(pointer_index, item)
+                return pointer_index + current_pointer_poition
+            continue
 
 def move_right_pointer(disk_map:list, current_pointer_poition:int) -> int:
     for pointer_index in range(current_pointer_poition - 1, -1, -1):
 
         if disk_map[pointer_index] != ".":
-            print(pointer_index,disk_map[pointer_index])
+            # print(pointer_index,disk_map[pointer_index])
             return pointer_index
 
+
+
+def swap_items_at_pointers(left:int, right:int, disk_map:list) -> list:
+    disk_map[left],disk_map[right] = disk_map[right],disk_map[left]
+
+    # print(f"SWAPED {disk_map[left]} AND {disk_map[right]}")
+    return disk_map
 
 
 
@@ -42,27 +50,38 @@ def reorder_disk_map(disk_map:list) -> list:
     left_pointer = 0
     right_pointer = int(len(disk_map))
 
-    left_pointer = move_left_pointer(disk_map,left_pointer)
-    right_pointer = move_right_pointer(disk_map,right_pointer)
+    while True:
+            left_pointer = move_left_pointer(disk_map,left_pointer)
+            right_pointer = move_right_pointer(disk_map,right_pointer)
+
+            if left_pointer > right_pointer:
+                del disk_map[right_pointer + 1:]
+                return disk_map
+
+            disk_map = swap_items_at_pointers(left_pointer,right_pointer,disk_map)
     
 
+def calculate_check_sum(disk_map):
+    total_check_sum = 0
+    for index, item in enumerate(disk_map):
+        total_check_sum += index * item
+    print(f'TOTAL CHECKSUM = {total_check_sum}')
 
 
 
 def main(content):
-    disk_map = generate_disk_map("12345")
-    print(disk_map)
+    disk_map = generate_disk_map(file_input=content)
+    print(f"\nINPUT = {disk_map}")
 
-    disk_map = reorder_disk_map(disk_map)
+    reordered_disk_map = reorder_disk_map(disk_map)
+    print(f"OUTPUT = {reordered_disk_map}\n---------------------")
+
+    calculate_check_sum(reordered_disk_map)
     
-    
-
-
-
 
 if __name__ == "__main__":
     here = os.path.dirname(__file__)
-    with open("../input/sample9.txt","r") as file:
+    with open(f"{here}/../input/sample9.txt","r") as file:
         content = file.read()
 
     main(content)
