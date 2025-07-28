85d3660cc34783e8e72dcc42fcf5ed6b2e094c33
Tom Horsman <thomashorsman99@gmail.com>
2025-07-28 17:19:01 +0100

    create 2 fundtions to help reorder/defragment the disk map: 1: move_left_pointer(), this starts at the left most or nth blank space in the diskmap and when called, moves the pointer to next avliable blank space 2: move_right_poiter(), this starts at the end of the diskmap, and when called moves left to the next avliable non blank space.

--- week_2/day_09/code/d9_p1.py
+++ week_2/day_09/code/d9_p1.py
@@ -1,11 +1,13 @@
+
 import os,sys
 
 
 def generate_disk_map(file_input:str) -> list:
     disk_map: list = []
     data_index = 0
-    for char_index, character in enumerate(file_input):
 
+    for char_index, character in enumerate(file_input):
+        
         if char_index % 2 == 1: 
             for i in range(int(character)):
                 disk_map.append(".")
@@ -18,6 +20,43 @@ def generate_disk_map(file_input:str) -> list:
     return disk_map
 
 
+def move_left_pointer(disk_map:list, current_pointer_poition:int) -> int:
+        for pointer_index, item in enumerate(disk_map[current_pointer_poition:]):
+            if item == ".":
+                print(pointer_index, item)
+                return pointer_index
+
+
+def move_right_pointer(disk_map:list, current_pointer_poition:int) -> int:
+    for pointer_index in range(current_pointer_poition - 1, -1, -1):
+
+        if disk_map[pointer_index] != ".":
+            print(pointer_index,disk_map[pointer_index])
+            return pointer_index
+
+
+
+
+
+def reorder_disk_map(disk_map:list) -> list:
+    left_pointer = 0
+    right_pointer = int(len(disk_map))
+
+    left_pointer = move_left_pointer(disk_map,left_pointer)
+    right_pointer = move_right_pointer(disk_map,right_pointer)
+    
+
+
+
+
+def main(content):
+    disk_map = generate_disk_map("12345")
+    print(disk_map)
+
+    disk_map = reorder_disk_map(disk_map)
+    
+    
+
 
 
 
@@ -26,5 +65,5 @@ if __name__ == "__main__":
     with open("../input/sample9.txt","r") as file:
         content = file.read()
 
-    disk_map = generate_disk_map(content)
-    print(disk_map)
+    main(content)
+   
