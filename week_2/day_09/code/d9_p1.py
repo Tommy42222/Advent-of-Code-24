1f7041e9257c5fc8da3185ac98ab61251f090ede
Tom Horsman <thomashorsman99@gmail.com>
2025-07-28 20:44:50 +0100

    - Reformat file for better readability - add code comments - add program run time counter

--- week_2/day_09/code/d9_p1.py
+++ week_2/day_09/code/d9_p1.py
@@ -1,5 +1,4 @@
-
-import os
+import os,time
 
 
 def generate_disk_map(file_input:str) -> list:
@@ -8,37 +7,35 @@ def generate_disk_map(file_input:str) -> list:
 
     for char_index, character in enumerate(file_input):
 
-        if char_index % 2 == 1: 
-            for i in range(int(character)):
-                disk_map.append(".")
-        
-        else:
+        if char_index % 2 == 0: 
             for i in range(int(character)):
                 disk_map.append(int(data_index))
             data_index += 1
-
+    
+        else:
+            for i in range(int(character)):
+                disk_map.append(".")
+                
     return disk_map
 
 
 
-def move_left_pointer(disk_map:list, current_pointer_poition:int) -> int:
+def move_left_pointer(disk_map:list, current_pointer_poition:int) -> int: # when called, jump the L_pointer right to the next empty space "."
         for pointer_index, item in enumerate(disk_map[current_pointer_poition:]):
             if item == ".":
-                # print(pointer_index, item)
                 return pointer_index + current_pointer_poition
-            continue
 
-def move_right_pointer(disk_map:list, current_pointer_poition:int) -> int:
-    for pointer_index in range(current_pointer_poition - 1, -1, -1):
 
+
+def move_right_pointer(disk_map:list, current_pointer_poition:int) -> int: # when called, jump the R_pointer left to the next filled space
+    for pointer_index in range(current_pointer_poition - 1, -1, -1): # starts at the -1 index and iterates right to left thorugh the list
         if disk_map[pointer_index] != ".":
-            # print(pointer_index,disk_map[pointer_index])
             return pointer_index
 
 
 
-def swap_items_at_pointers(left:int, right:int, disk_map:list) -> list:
-    disk_map[left],disk_map[right] = disk_map[right],disk_map[left]
+def swap_items_at_pointers(left:int, right:int, disk_map:list) -> list: 
+    disk_map[left],disk_map[right] = disk_map[right],disk_map[left] 
 
     # print(f"SWAPED {disk_map[left]} AND {disk_map[right]}")
     return disk_map
@@ -47,42 +44,52 @@ def swap_items_at_pointers(left:int, right:int, disk_map:list) -> list:
 
 
 def reorder_disk_map(disk_map:list) -> list:
-    left_pointer = 0
-    right_pointer = int(len(disk_map))
+    left_pointer = 0                     # start at the beginning of the disk_map
+    right_pointer = int(len(disk_map))   # start at the end of the disk_map
 
     while True:
             left_pointer = move_left_pointer(disk_map,left_pointer)
             right_pointer = move_right_pointer(disk_map,right_pointer)
 
-            if left_pointer > right_pointer:
-                del disk_map[right_pointer + 1:]
+            if left_pointer > right_pointer:      # the disk is sorted when the two pointers cross each other.
+                del disk_map[right_pointer + 1:]  # once sorted, remove all "." after after the r_pointer to save space
                 return disk_map
-
-            disk_map = swap_items_at_pointers(left_pointer,right_pointer,disk_map)
+            
+            else:
+                disk_map = swap_items_at_pointers(left_pointer,right_pointer,disk_map)
     
 
-def calculate_check_sum(disk_map):
+
+def calculate_check_sum(disk_map:list) -> int:
     total_check_sum = 0
     for index, item in enumerate(disk_map):
         total_check_sum += index * item
-    print(f'TOTAL CHECKSUM = {total_check_sum}')
+    return total_check_sum
 
 
 
-def main(content):
+def main(content:str) -> None:
     disk_map = generate_disk_map(file_input=content)
-    print(f"\nINPUT = {disk_map}")
+    # print(f"\nINPUT = {disk_map}")
 
     reordered_disk_map = reorder_disk_map(disk_map)
-    print(f"OUTPUT = {reordered_disk_map}\n---------------------")
+    # print(f"OUTPUT = {reordered_disk_map}\n---------------------")
 
-    calculate_check_sum(reordered_disk_map)
+    final_checksum = calculate_check_sum(reordered_disk_map)
+    print(f'TOTAL CHECKSUM = {final_checksum}')
+
     
+
+
 
 if __name__ == "__main__":
     here = os.path.dirname(__file__)
-    with open(f"{here}/../input/sample9.txt","r") as file:
+    with open(f"{here}/../input/input9.txt","r") as file:
         content = file.read()
-
+    
+    print("PROGRAM RUNNING...")
+    start = time.time()
     main(content)
-   
+    end = time.time()
+
+    print(f"PROGRAM FINISHED!!\nTOTAL RUN TIME ={end-start:.2f}s")
