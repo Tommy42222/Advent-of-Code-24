2696f3f3364a71bc2b598ffdc51758a76c36d1c2
Tom Horsman <thomashorsman99@gmail.com>
2025-08-05 15:57:45 +0100

    working soltion for part 1

--- week_2/day_10/code/d10_p1.py
+++ week_2/day_10/code/d10_p1.py
@@ -13,17 +13,21 @@ def find_all_starting_locs(grid): # this returns a list of tuples (y_cord,x_cord
             if item == "0":
                 starting_locations.append((row_index,item_index))
                 
-    print(starting_locations)
+    # print(starting_locations)
     return starting_locations
                 
 
 
-def check_neighbours(current_location: tuple, stack: list, grid:list) -> set:
+def check_neighbours(current_location: tuple, to_vist_stack: set, visted:list, grid:list) -> set:
+    print("---")
     y_cord = current_location[0]
     x_cord = current_location[1]
+    # print(f"input = {(y_cord,x_cord)}")
+
 
     current_hight = int(grid[y_cord][x_cord])
 
+    # these start at north and go clockwise
     y_change = [1,0,-1,0]
     x_change = [0,1,0,-1]
 
@@ -32,31 +36,65 @@ def check_neighbours(current_location: tuple, stack: list, grid:list) -> set:
         new_y_cord = y_cord + y_change[direction]
 
         if new_x_cord < 0 or new_y_cord < 0:
-            print("loop around")
+            # print(loc,"loop around")
             continue
 
         try:
-            if int(grid[new_y_cord][new_x_cord]) == current_hight + 1:
-                # print(grid[new_y_cord][new_x_cord])  
-                stack.append((new_y_cord,new_x_cord))
-        
+            loc = (new_y_cord,new_x_cord)
+            if loc in visted:
+                # print(loc,"visited")
+                continue
 
-        except IndexError:
-            print('out of bounds')
+            if int(grid[new_y_cord][new_x_cord]) == current_hight + 1:
+            
+                # print((new_y_cord,new_x_cord),"added")  
+                to_vist_stack.add((loc))
+        
+        except:
+            # print(loc,'error')
             continue
     
-    print(stack)
-    return stack
+    # print("tv",to_vist_stack)
+    # print("v",visted)
+    return to_vist_stack
 
+
+
+def dfs(location: tuple ,to_vist: set[tuple], visted: list, grid:list):
+    counter = 0
+
+    visted.append(location)
+    to_vist = check_neighbours(location,to_vist,visted,grid)
+
+    while to_vist:
+
+        location = to_vist.pop()
+        
+        if grid[location[0]][location[1]] == "9":
+            print("FOUND A 9!")
+            counter += 1
+
+        visted.append(location)
+        to_vist = check_neighbours(location,to_vist,visted,grid)
+
+
+    print(f"final count {counter}")
+    return counter
 
 
 def main() -> None:
     grid = parse_input(content)
+    pprint(grid)
     starting_locations = find_all_starting_locs(grid)
-    visted = []
 
-    for sl in starting_locations:
-        check_neighbours(sl,visted,grid)
+    count = 0
+
+    for location in starting_locations:
+        to_vist = set()
+        visted = []
+        count += dfs(location,to_vist,visted,grid)
+
+    print(count)
 
 
 
