4a4156776fffe4a5e7c3082e9525ba799080699e
Tom Horsman <thomashorsman99@gmail.com>
2025-08-11 16:28:50 +0100

    add read_file()

--- functions/homeGrownFunctions.py
+++ functions/homeGrownFunctions.py
@@ -36,3 +36,19 @@ def openFile(fileName): # takes in a file name, opens it, and returns the file a
     
 def output_2d_list_as_string(grid,seperator):
     print('\n'.join(map(seperator.join, grid)))
+
+def read_file(option:str, day_number:int) -> str:
+    here = os.path.dirname(__file__)
+    content = "ERROR WHEN READING FILE "
+    
+    if option == "i":
+        with open(f"{here}/../input/input{day_number}.txt", "r") as file:
+            content = file.read()
+
+    elif option == "s":
+        with open(f"{here}/../input/sample{day_number}.txt", "r") as file:
+            content = file.read()
+    else:
+        raise ValueError(f"Input must be either 's' or 'i'... You entered '{option}' <<< shame on you...")
+
+    return content
