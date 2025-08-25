6d2a8722700b012955322de79388451936aab706
Tom Horsman <thomashorsman99@gmail.com>
2025-08-25 21:25:41 +0100

    add basic boiller plate

---
+++ week_2/day_14/code/d14_p1.py
@@ -0,0 +1,26 @@
+import os
+
+
+def read_file(option:str, day_number:int) -> str:
+    here = os.path.dirname(__file__)
+    content = "ERROR WHEN READING FILE "
+    
+    if option == "i":
+        with open(f"{here}/../input/input{day_number}.txt", "r") as file:
+            content = file.read()
+
+    elif option == "s":
+        with open(f"{here}/../input/sample{day_number}.txt", "r") as file:
+            content = file.read()
+    else:
+        raise ValueError(f"Input must be either 's' or 'i'... You entered '{option}' <<< shame on you...")
+
+    return content
+
+def main()-> None:
+    content = read_file("s","14")
+    print(content)
+
+
+if __name__ == "__main__":
+    main()
