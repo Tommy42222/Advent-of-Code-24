e346b53549d85db1e978d7459ae61b474a3c19b0
Tom Horsman <thomashorsman99@gmail.com>
2025-08-25 20:54:54 +0100

    Working solution for part 1 ADD:     - calculate_values()

--- week_2/day_13/code/d13_p1.py
+++ week_2/day_13/code/d13_p1.py
@@ -34,25 +34,50 @@ def get_next_machine_input(input_content:list[str]) -> namedtuple :
     y_values = re.findall(y_values_pattern,machine)
     
     # removes the X= and Y= prefix's from each iten in the lists
-    x_values = [sub[2:] for sub in x_values]
-    y_values = [sub[2:] for sub in y_values]
+    x_values = [int(sub[2:]) for sub in x_values]
+    y_values = [int(sub[2:]) for sub in y_values]
     
     button_A = Values(x_values[0],y_values[0])
     button_B = Values(x_values[1],y_values[1])
     targets = Values(x_values[2],y_values[2])
 
+
     return button_A, button_B, targets, input_content
     
 
-def main() -> None:
-    _content = read_file("s","13").splitlines()
+def calculate_values(a_button:namedtuple,b_button:namedtuple,targets:namedtuple):
+    output = 0
+
+    denominator = (a_button.x_value * b_button.y_value - a_button.y_value * b_button.x_value)
+
+    if denominator == 0:
+        return 0 
     
+    numerator_A = ((targets.x_value * b_button.y_value - targets.y_value * b_button.x_value))
+    numerator_B = ((a_button.x_value * targets.y_value - a_button.y_value * targets.x_value))
+
+    if numerator_A % denominator != 0 or numerator_B % denominator != 0:
+        return 0
+
+    button_A = numerator_A // denominator
+    button_B = numerator_B // denominator
+
+    if 0 <= button_A <= 100 and 0 <= button_B <= 100:
+        output = (button_A * 3) + (button_B * 1)
+        print(f"{button_A = }, {button_B = }, {output = }")
+    return output
+
+
+def main() -> None:
+    _content = read_file("i","13").splitlines()
+    final_sum = 0
+
     while len(_content) > 0: # for each machine in _content
         button_A,button_B,targets,_content = get_next_machine_input(_content)
-        
-        
-        print(button_A,button_B,targets)
-        
+        final_sum += calculate_values(button_A,button_B,targets)
+
+
+    print(f"Final token cost = {final_sum}")  
 
 if __name__ == "__main__":
     main()
