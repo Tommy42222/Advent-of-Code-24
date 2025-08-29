2d8cf8d9363496cb0bf3c5284017f1e176dc7c42
Tom Horsman <thomashorsman99@gmail.com>
2025-08-29 15:25:53 +0100

    working solution for day 15 part 1

--- week_3/day_15/code/d15_p1.py
+++ week_3/day_15/code/d15_p1.py
@@ -1,6 +1,8 @@
-import os
+import os,time
 from pprint import pprint
 from collections import namedtuple
+
+
 
 def read_file(option:str, day_number:int) -> str:
     here = os.path.dirname(__file__)
@@ -19,10 +21,13 @@ def read_file(option:str, day_number:int) -> str:
     return content
 
 
+
 def find_blank(_content) -> int:
     for index,item in enumerate(_content):
         if item == "":
             return index
+
+
 
 def parse_input(_content:list) -> tuple[list[str],str]:
     id = find_blank(_content)
@@ -33,6 +38,7 @@ def parse_input(_content:list) -> tuple[list[str],str]:
     return area_map,instructions
 
 
+
 def get_bot_starting_position(area_map:list) -> tuple[int,int]:
     Coordinates = namedtuple("Coordinates",("py","px"))
     for row_index, row in enumerate(area_map):
@@ -41,33 +47,95 @@ def get_bot_starting_position(area_map:list) -> tuple[int,int]:
                 return Coordinates(row_index,item_index)
 
 
-def move_bot(area_map:list[str],movement_vector:tuple[int,int], bot_postion:tuple[int,int]) -> tuple[list[str],tuple[int,int]]:
+
+def move_bot(area_map:list[str],movement_vector:namedtuple, bot_postion:namedtuple) -> tuple[list[str],namedtuple]:
+    Coordinates = namedtuple("Coordinates",("py","px"))
+    area_map[bot_postion.py][bot_postion.px] = "."
+
+    dx = bot_postion.px + movement_vector.vx
+    dy = bot_postion.py + movement_vector.vy
     
-    ...
+    next_square = area_map[dy][dx]
+    next_square_position = Coordinates(dy,dx)
+
+    # print(f"{next_square = } | {bot_postion.py,bot_postion.px} ->" ,end=" ")
+
+    match next_square: 
+        case ".": # empty space
+            print("moving")
+            bot_postion = next_square_position
+
+        case "O": # box
+            print("pushing box")
+            area_map, bot_postion = push_box(area_map=area_map,box_location=next_square_position,robot_position=bot_postion,vectors=movement_vector)
+
+        case "#": # wall
+            print("hit wall")
+            pass
+    
+    # print(f"{bot_postion.py,bot_postion.px}")
+    area_map[bot_postion.py][bot_postion.px] = "@"
+    return area_map, bot_postion
+
+
+
+def push_box(area_map:list[str],box_location:namedtuple, robot_position:namedtuple, vectors:namedtuple) -> tuple[list[str],namedtuple]:
+    next_square = "0"
+    dx = box_location.px + vectors.vx
+    dy = box_location.py + vectors.vy
+    while next_square != "#":
+
+        next_square = area_map[dy][dx]
+        if next_square == ".":
+
+            area_map[dy][dx],area_map[box_location.py][box_location.px] = area_map[box_location.py][box_location.px],area_map[dy][dx]
+            robot_position = box_location
+
+            return area_map,robot_position
+
+        dx = dx + vectors.vx
+        dy = dy + vectors.vy
+    
+    print("no room to push")
+    return area_map,robot_position
+
+
+
+def calculate_checksum(area_map:list[str]) -> int:
+    checksum = 0
+    for row_index, row in enumerate(area_map):
+        for item_index, item in enumerate(row):
+            if item == "O":
+                checksum += 100 * row_index + item_index
+                # print(checksum)
+    return checksum
+
 
 
 def main()-> None:
-    _content = read_file("s","15").splitlines()
+    _content = read_file("i","15").splitlines()
 
+    # parse input 
     area_map, instructions = parse_input(_content)
     bot_postion:namedtuple[int,int] = get_bot_starting_position(area_map)
 
-    # remove the bot from the area map as to prevent errros if the bot try's and move back onto its starting square
-    area_map[bot_postion.py][bot_postion.px] = "."
-
     # each namedtuple is formated (y-cord,x-cord)
     Vector = namedtuple("Vectors",("vy","vx"))
-    direction_values: dict[str:tuple[int,int]] = {"^":Vector(1,0), ">":Vector(0,1), "v":Vector(-1,0), "<":Vector(0,-1), None:Vector(0,0)}
+    direction_values: dict[str:tuple[int,int]] = {"^":Vector(-1,0), ">":Vector(0,1), "v":Vector(1,0), "<":Vector(0,-1)}
+    final_checksum = 0
 
     for command in instructions:
-
         directional_vector = direction_values[command]
         area_map,bot_postion = move_bot(area_map,directional_vector,bot_postion)
-        
-        # print(command,end=" ")
-        # print((direction_values[command].vy,direction_values[command].vx))
-        ...
+
+
+    final_checksum = calculate_checksum(area_map)
+    print(f"{final_checksum =:,}")
+
 
 
 if __name__ == "__main__":
+    s = time.time()
     main()
+    f = time.time()
+    print(f"TOTAL RUN TIME = {f-s:.3f}s")
